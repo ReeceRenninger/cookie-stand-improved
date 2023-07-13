6db9c3a287129b2bbc7f4c942c3582db259f7b03
MICHAEL STEOGER <ikesteoger@gmail.com>
2023-07-13 13:47:49 -0700

    tried to do math, dont know how

--- app/Components/Form.js
+++ app/Components/Form.js
@@ -1,8 +1,8 @@
 "use client";
 import { useState } from 'react';
-import Store from './Store';
+// import Store from './Store';
 
-const Form = () => {
+const Form = ({ allStores, setAllStores }) => {
 
   const [name, setName] = useState('');
   const [minCust, setMinCust] = useState(0);
@@ -11,40 +11,40 @@ const Form = () => {
   const [showForm, setShowForm] = useState(false);
 
   const handleSubmit = (e) => {
-    e.preventDefault();
-    props.setStores([...props.stores, new Store(name, minCust, maxCust, avgCookie)]);
+    // e.preventDefault();
+    setAllStores([...allStores, [name, minCust, maxCust, avgCookie]]);
     setShowForm(false);
     }
 
     return (
         <>
         {/* tried setting to true but was acting wonky so trying to use showForm */}
-        <button onClick={() => setShowForm(!showForm)} class="bg-transparent hover:bg-blue-500 text-blue-700 font-semibold hover:text-white py-2 px-4 border border-blue-500 hover:border-transparent rounded">Add Store</button>
+        <button onClick={() => setShowForm(!showForm)} className="bg-transparent hover:bg-blue-500 text-blue-700 font-semibold hover:text-white py-2 px-4 border border-blue-500 hover:border-transparent rounded">Add Store</button>
         {showForm && (
-        <form class="w-full max-w-sm" id="store-form">
+        <form className="w-full max-w-sm" id="store-form">
             <fieldset>
                 {/* <legend id="fieldsettitle">Add A Store</legend> */}
                 <section>
-                    <label class="block text-gray-500 font-bold mb-1 md:mb-0 pr-4" for="city-input">City of Store (required)</label>
-                    <input type="text" id="city-input" name="store_city" placeholder="Seattle" required class="text-slate-950" 
+                    <label className="block text-gray-500 font-bold mb-1 md:mb-0 pr-4">City of Store (required)</label>
+                    <input type="text" placeholder="Seattle" required className="text-slate-950" 
                     onChange={(e) => setName(e.target.value)}/>
                 </section>
                 <section>
-                    <label class="block text-gray-500 font-bold mb-1 md:mb-0 pr-4" for="min-input">Minimum Cookies Per Hour (required)</label>
-                    <input type="number" id="min-input" name="minCust" placeholder="2" required min="0" class="text-slate-950" 
+                    <label className="block text-gray-500 font-bold mb-1 md:mb-0 pr-4">Minimum Cookies Per Hour (required)</label>
+                    <input type="number" placeholder="2" required min="0" className="text-slate-950" 
                     onChange={(event) => setMinCust(+event.target.value)}/>
                 </section>
                 <section>
-                    <label class="block text-gray-500 font-bold mb-1 md:mb-0 pr-4" for="max-input">Maximum Cookies Per Hour (required)</label>
-                    <input type="number" id="max-input" name="maxCust" placeholder="42" required min="0" class="text-slate-950" 
+                    <label className="block text-gray-500 font-bold mb-1 md:mb-0 pr-4">Maximum Cookies Per Hour (required)</label>
+                    <input type="number" placeholder="42" required min="0" className="text-slate-950" 
                     onChange={(e) => setMaxCust(+e.target.value)}/>
                 </section>
                 <section>
-                    <label class="block text-gray-500 font-bold mb-1 md:mb-0 pr-4" for="avg-input">Avg Cookie Per Customer (required)</label>
-                    <input type="number" id="avg-input" name="store_avg" placeholder="5.9" required min="0" maxLength="10" class="text-slate-950" 
+                    <label className="block text-gray-500 font-bold mb-1 md:mb-0 pr-4">Avg Cookie Per Customer (required)</label>
+                    <input pattern="^(\d*\.)?\d+$" placeholder="5.9" min="0.0" required  className="text-slate-950" 
                     onChange={(e) => setAvgCookie(+e.target.value)}/>
                 </section>
-                <button className="text-slate-950" type='submit' onClick={() => handleSubmit()} class="bg-transparent hover:bg-blue-500 text-blue-700 font-semibold hover:text-white py-2 px-4 border border-blue-500 hover:border-transparent rounded">Add Store to Table!</button>
+                <button className="bg-transparent hover:bg-blue-500 text-blue-700 font-semibold hover:text-white py-2 px-4 border border-blue-500 hover:border-transparent rounded" type='submit' onClick={() => handleSubmit()}>Add Store to Table!</button>
             </fieldset>
         </form>
         )}
