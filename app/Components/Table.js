e5dc255ff705fecdbed75950bd38f06d8e5afba0
KaedenOC <komeacarr2@gmail.com>
2023-07-12 18:04:06 -0700

    playing with some stuff

--- app/Components/Table.js
+++ app/Components/Table.js
@@ -1,7 +1,28 @@
+// import Header from "./Header";
+// const hourArray = ["6 am", "7 am", "8 am", "9 am", "10 am", "11 am", "12 am", "1 pm", "2 pm", "3 pm", "4 pm", "5 pm", "6 pm", "7 pm"];
+
+// const LocationData = ({ name, minCust, maxCust, avgCookieSale }) => {
+//     const [cookiesPerHour, setCookiesPerHour] = useState([]);
+//     const [custPerHour, setCustPerHour] = useState([]);
+//     const [totalCookies, setTotalCookies] = useState(0);
+// }
+
+// implement the rngCust function
+// getCustPerHour function 
+// const custperhourarray = hourArray.map(()=> {})
+// getCookiesPerHour fuction
+// will useEffect be useful?
+
 const Table = () => {
     return (
         <table>
             <p>This is a table?</p>
+            {/* <Header hourArray={hourArray} /> */}
+            {/* <tbody>
+                {store.map((location, index) => {
+                    <LocationData key={index} location={location} hourArray={hourArray} />
+                })}
+            </tbody> */}
         </table>
     );
 }
--- app/page.js
+++ app/page.js
@@ -13,6 +13,7 @@ export default function Home() {
       <Table />
       {/* <Store /> */}
       <Form />
+      <Footer />
     </main>
     </>
   )
